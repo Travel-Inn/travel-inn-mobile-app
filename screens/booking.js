61011bfca554caf2401e0bd0ad9dac37180e9680
Wonder <samuel.nai@yahoo.com>
2022-05-10 00:12:03 +0000

    add components and style to booking screen

--- screens/booking.js
+++ screens/booking.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import { useState } from 'react';
-import { Text, View, StyleSheet, ImageBackground, TouchableOpacity, Dimensions } from 'react-native';
+import { Text, View, Image, StyleSheet, ImageBackground, TouchableOpacity, Dimensions, ScrollView } from 'react-native';
 import { Input } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import AwesomeeIcon from 'react-native-vector-icons/FontAwesome5';
-import Firebase from '../config/firebase';
-import { registration } from '../config/firebase';
-import style from 'react-native-datepicker/style';
 
 
 export default function Booking({navigation}){
@@ -23,33 +19,57 @@ export default function Booking({navigation}){
 					<ImageBackground style={styles.pageImage} source={require("../images/home.jpg")}/>
 				</View>
 			</View>
-			<View style={styles.content}>
+			<ScrollView contentContainerStyle={styles.content}>
 				<View style={styles.searchform}>
 					<View style={{flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
 						<DatePicker
 							date={date}
 							mode="date"
 							placeholder="Check In"
-							style={{border: "1px solid black", marginRight: 10, borderRadius: 10}}
 							format="DD-MM-YYYY"
 							minDate="14-06-2022"
 							confirmBtnText="Confirm"
+							iconSource={null}
 							cancelBtnText="Cancel"
-							iconComponent={<Icon name="calendar-o" size={20} color="black" style={styles.dateIcon}/>}
+							iconComponent={<Icon name="calendar-o" size={20} color="white" style={styles.dateIcon}/>}
 							onDateChange={()=>setDate(date)}
+							customStyles={{
+								dateInput:{
+									backgroundColor: 'black',
+									color: 'white',
+									border: "1px solid black",
+									borderRadius: 10,
+									marginRight: 20
+								},
+								placeholderText:{
+									color: 'white',
+								}
+							}}
 						/>
 
 						<DatePicker
 							date={date}
 							mode="date"
+							iconSource={null}
 							placeholder="Check Out"
-							style={{border: "1px solid black", borderRadius: 10}}
 							format="DD-MM-YYYY"
 							minDate="14-06-2022"
 							confirmBtnText="Confirm"
 							cancelBtnText="Cancel"
-							iconComponent={<Icon name="calendar-o" size={20} color="black" style={styles.dateIcon}/>}
+							iconComponent={<Icon name="calendar-o" size={20} color="white" style={styles.dateIcon}/>}
 							onDateChange={()=>setDate(date)}
+							customStyles={{
+								dateInput:{
+									backgroundColor: 'black',
+									color: 'white',
+									border: "1px solid black",
+									borderRadius: 10,
+									marginRight: 10
+								},
+								placeholderText:{
+									color: 'white',
+								}
+							}}
 						/>
 					</View>
 					<Input
@@ -58,7 +78,16 @@ export default function Booking({navigation}){
 						keyboardType="numeric"
 						onChangeText={text => setName(text)}
 						placeholder = "Number of beds"
-						inputStyle = {{color: "black"}}
+						placeholderTextColor={"white"}
+						leftIcon={
+						<Icon name='users' size={20} color="white" 
+							style={{position: 'absolute', left: 15, top: 15, borderBottom: 'none'}}
+						/>}
+						inputStyle = {{
+							color: "white", textAlign: 'center', borderRadius: 10,
+							backgroundColor: 'black', marginTop: 10
+						}}
+						inputContainerStyle={{borderBottom: 'none'}}
 					/>
 					<View style={{flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
 						<Input
@@ -67,29 +96,62 @@ export default function Booking({navigation}){
 							keyboardType="numeric"
 							onChangeText={text => setMin(text)}
 							placeholder = "min"
-							inputStyle = {{color: "black"}}
-							style={{width: 20, border: "1px solid black", textAlign: 'center'}}
+							placeholderTextColor={'white'}
+							inputStyle = {{
+											backgroundColor: "black", textAlign: 'center', width: 50, 
+											border: "1px solid black", borderRadius: 10, color: 'white'
+										}}
+							inputContainerStyle={{borderBottom: "none"}}
 						/>
 						<Input
 							value={max}
 							textContentType="telephoneNumber"
 							keyboardType="numeric"
 							onChangeText={text => setMax(text)}
+							placeholderTextColor={'white'}
 							placeholder = "max"
-							inputStyle = {{color: "black"}}
-							style={{width: 20, border: "1px solid black", textAlign: 'center'}}
+							inputStyle = {{
+											backgroundColor: "black", 
+											textAlign: 'center', width: 50, 
+											border: "1px solid black", borderRadius: 10,
+											color: 'white'
+										}}
+							inputContainerStyle={{borderBottom: "none"}}
 						/>
 					</View>
-					<TouchableOpacity
+				</View>
+				<TouchableOpacity
 						onPress = {null}
 						color = "white"
 						style= {styles.continue}
 					>
-						<Text style={{color: 'white'}}>Continue</Text>
-					</TouchableOpacity>
+						<Text style={{fontWeight: 'bold'}}>Search</Text>
+				</TouchableOpacity>
+				<View style={styles.room}>
+					<Image source={require('../images/menu.jpg')} style={{flex: 2}} />
+					<View style={{flex: 4, justifyContent: 'space-around', alignItems: 'center'}}>
+						<Text>Single Room</Text>
+						<Text>Single Room with Single Bed</Text>
+						<Text>GHC 200.00</Text>
+					</View>
 				</View>
-			</View>
-			
+				<View style={styles.room}>
+					<Image source={require('../images/login.jpg')} style={{flex: 2}} />
+					<View style={{flex: 4, justifyContent: 'space-around', alignItems: 'center'}}>
+						<Text>Twin-Bed Room</Text>
+						<Text>Single Room with Twin Beds</Text>
+						<Text>GHC 300.00</Text>
+					</View>
+				</View>
+				<View style={styles.room}>
+					<Image source={require('../images/forgotten.jpg')} style={{flex: 2}} />
+					<View style={{flex: 4, justifyContent: 'space-around', alignItems: 'center'}}>
+						<Text>Double Room</Text>
+						<Text>Single Room with Double Bed</Text>
+						<Text>GHC 350.00</Text>
+					</View>
+				</View>
+			</ScrollView>
 		</View>
 	);
 }
@@ -101,7 +163,7 @@ const styles = StyleSheet.create({
 	imageContainer:{
 		flex: 2,
 		borderBottomRightRadius: Dimensions.get('screen').width*(50/100),
-		height: "80%",
+		minHeight: "35%",
 		borderBottomLeftRadius: Dimensions.get('screen').width*(50/100),
 		transform: [{scaleX: 2}],
 		overflow: 'hidden',
@@ -123,7 +185,8 @@ const styles = StyleSheet.create({
 		backgroundColor: 'white', 
 		width: Dimensions.get('screen').width*0.9,
 		padding: 10,
-		justifyContent: 'center',
+		marginTop: Dimensions.get('screen').height*0.4,
+		justifyContent: 'space-around',
 		alignItems: 'center',
 		borderRadius: 20
 	},
@@ -138,15 +201,26 @@ const styles = StyleSheet.create({
 		backgroundColor: 'black',
 		paddingLeft: 15,
 		paddingRight: 15,
+		overflow: 'scroll'
 	},
 	continue:{
 		padding: 12,
-		borderRadius: 15,
+		borderRadius: 25,
 		paddingLeft: 15,
 		paddingRight: 15,
-		backgroundColor: 'black',
-		width: '60%',
+		marginTop: 20,
+		marginBottom: 20,
+		backgroundColor: 'white',
+		width: '40%',
 		alignItems: 'center',
 		justifyContent: 'center'
+	},
+	room:{
+		width: Dimensions.get('screen').width*0.85,
+		flexDirection: 'row',
+		backgroundColor: 'white',
+		borderRadius: 20,
+		height: Dimensions.get('screen').height*0.15,
+		marginBottom: 20
 	}
 });
