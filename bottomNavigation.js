1f6de7c314d7ad3f1eb669099ea56c99922fa58c
Wonder <samuel.nai@yahoo.com>
2022-05-12 02:46:40 +0000

    add screens to bottom navigation

--- bottomNavigation.js
+++ bottomNavigation.js
@@ -4,12 +4,15 @@ import Booking from './screens/booking';
 import Contact from './screens/contact';
 import Home from './screens/home';
 import Menu from './screens/menu';
+import Profile from './screens/profile';
+import Room from './screens/room';
+import UserDetails from './screens/userDetails';
 
 export default function BottomTabs(){
     const Tab = createBottomTabNavigator();
     
     return(
-        <Tab.Navigator initialRouteName='Home' screenOptions={{headerShown: false, tabBarShowLabel: false}}>
+        <Tab.Navigator initialRouteName='Home' backBehavior='history' screenOptions={{headerShown: false, tabBarShowLabel: false}}>
             <Tab.Screen name="Home" component={Home} options={{tabBarIcon: ()=>(
                 <Icon name="home" size={20} color = "black"/>
             )}}/>
@@ -19,9 +22,12 @@ export default function BottomTabs(){
             <Tab.Screen name="Booking" component={Booking} options={{tabBarIcon: ()=>(
                 <Icon name="briefcase" size={20} color = "black"/>
             )}}/>
-            <Tab.Screen name="Contact" component={Contact} options={{tabBarIcon: ()=>(
+            <Tab.Screen name="Profile" component={Profile} options={{tabBarIcon: ()=>(
                 <Icon name="user" size={20} color = "black"/>
             )}}/>
+            <Tab.Screen name="Contact" component={Contact} options={{tabBarButton:()=>null, tabBarVisible: false}} />
+            <Tab.Screen name="User" component={UserDetails} options={{tabBarButton:()=>null, tabBarVisible: false}} />
+            <Tab.Screen name="Room" component={Room} options={{tabBarButton:()=>null, tabBarVisible: false}} />
         </Tab.Navigator>
     )
 }
