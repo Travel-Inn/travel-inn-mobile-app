751503e8ddc9464261a478cb6e3c5c108e9a239f
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-05-23 11:49:47 +0000

    Returned routes

--- App.js
+++ App.js
@@ -2,14 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
 import firebase from 'firebase/compat/app';
 import apiKeys from './config/keys';
-import SignupPage from './screens/signupPage.js';
-import LoginPage from './screens/loginPage.js';
-import ForgottenPasswordPage from './screens/forg_Pass.js';
-import ResetPasswordPage from './screens/resetPassword.js';
-import BottomTabs from './bottomNavigation';
+import Routes from './navigation';
 
 
 
@@ -22,14 +17,6 @@ export default function App() {
 
 
 	return (
-		<NavigationContainer >
-		  <Stack.Navigator initialRouteName='BottomNav' backBehavior="history" screenOptions={{headerShown: false}}>
-			<Stack.Screen name="Signup" component={SignupPage}/>
-			<Stack.Screen name="Login" component={LoginPage} />
-			<Stack.Screen name="Forgotten Password" component={ForgottenPasswordPage}/>
-			<Stack.Screen name="Reset Password" component={ResetPasswordPage}/>
-			<Stack.Screen name="BottomNav" component={BottomTabs}/>
-		  </Stack.Navigator>
-		</NavigationContainer>
+		<Routes/>
 	);
 }
