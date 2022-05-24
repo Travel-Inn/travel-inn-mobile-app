adc8e3fe71c774193a03eacb4a31ed22983894e0
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-05-24 07:32:04 +0000

    Changed isloading to loading to achieve consistency

--- navigation/rootNavigation.js
+++ navigation/rootNavigation.js
@@ -13,14 +13,14 @@ const auth = Firebase.auth();
 
 export default function RootNavigator() {
   const { user, setUser } = useContext(AuthenticatedUserContext);
-  const [isLoading, setIsLoading] = useState(true);
+  const [Loading, setLoading] = useState(true);
 
   useEffect(() => {
     // onAuthStateChanged returns an unsubscriber
     const unsubscribeAuth = auth.onAuthStateChanged(async authenticatedUser => {
       try {
         await (authenticatedUser ? setUser(authenticatedUser) : setUser(null));
-        setIsLoading(false);
+        setLoading(false);
       } catch (error) {
         console.log(error);
       }
@@ -30,7 +30,7 @@ export default function RootNavigator() {
     return unsubscribeAuth;
   }, []);
 
-  if (isLoading) {
+  if (Loading) {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <ActivityIndicator size='large' />
