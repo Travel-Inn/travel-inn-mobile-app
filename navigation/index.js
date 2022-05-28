1786d61f7cc2916e000e7c67e9fe8725bcaa45d8
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-05-28 06:09:46 +0000

    Minor fixes

--- navigation/index.js
+++ navigation/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Toast from 'react-native-toast-message'
-
+import Toast from 'react-native-toast-message';
 import { AuthenticatedUserProvider } from './AuthenticatedUserProvider';
 import RootNavigator from './rootNavigation';
 
 /**
  * Wrap all providers here
  */
-console.log("Was this executed");
 
 export default function Routes() {
   return (
