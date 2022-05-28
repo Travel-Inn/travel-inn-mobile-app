e2a69f217e5ad3d7d1c5291b132823a210686ed8
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-05-28 06:16:16 +0000

    Minor fixes

--- widgets/toastNotification.js
+++ widgets/toastNotification.js
@@ -1,9 +1,9 @@
 import Toast from 'react-native-toast-message';
 
 export function errorToastNotifier(text1, text2){
-     if (text2.length > 60){
-        const firstHalf = text2.substring(0,58);
-        const secondHalf = text2.substring(58,);
+     if (text2.length > 40){
+        const firstHalf = text2.substring(0,40);
+        const secondHalf = text2.substring(40,);
         const temp = firstHalf + '-\n' + secondHalf;
         return Toast.show({
             type:"error",
