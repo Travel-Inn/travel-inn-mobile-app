3c62b9c2774502c0a41852496c672954fac1b800
Christian Abrokwa <Cabrokwa11@gmail.com>
2022-05-28 05:42:59 +0000

    Refined toast notificaiton function

--- widgets/toastNotification.js
+++ widgets/toastNotification.js
@@ -1,9 +1,41 @@
 import Toast from 'react-native-toast-message';
 
-export function toastNotifier(){
+export function errorToastNotifier(text1, text2){
+     if (text2.length > 60){
+        const firstHalf = text2.substring(0,58);
+        const secondHalf = text2.substring(58,);
+        const temp = firstHalf + '-\n' + secondHalf;
+        return Toast.show({
+            type:"error",
+            text1: text1,
+            text2: temp,
+            visibilityTime: 10000,
+        })
+    }else {
     return Toast.show({
-        type: 'error',
-        text1: 'This is an info message',
-        text2: "My name is chiki"
+        type: 'success',
+        text1: text1,
+        text2: text2,
         });
+    }
+};
+
+export function successfulToastNotifier(text1, text2){
+    if (text2.length > 60){
+        const firstHalf = text2.substring(0,58);
+        const secondHalf = text2.substring(58,);
+        const temp = firstHalf + '-\n' + secondHalf;
+        return Toast.show({
+            type:"success",
+            text1: text1,
+            text2: temp,
+            visibilityTime: 10000,
+        })
+    }else {
+    return Toast.show({
+        type: 'success',
+        text1: text1,
+        text2: text2,
+        });
+    }
 };
