0125b2279145eb7b1ca7710aeadb6bd24ccc2c4e
Azicoco <mikeaziawa@gmail.com>
2022-06-02 14:49:12 +0000

    splash screen

--- screens/home/front.js
+++ screens/home/front.js
@@ -33,7 +33,7 @@ export default function Front({navigation}){
 
     const styles =StyleSheet.create({
         container:{
-            flex:1,
+            flex:2,
             padding:50,
             backgroundColor:"#1b1a1a",
             
